bb423f359cdde721a1136c1840bfb7818c4e31bd
TechAZsure <148707039+TechAZsure@users.noreply.github.com>
2024-01-20 11:36:02 +0530

    Add files via upload

---
+++ Index/databases/3d-print.php
@@ -0,0 +1,25 @@
+<?<php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$number = $_POST['number'];
+$date = $_POST['date'];
+$message = $_POST['details'];
+
+$email_from = 'rt24projects.24@techazsure.com';
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $visitor_email.\n".
+                "User Number: $number.\n".
+                "User Deadline: $date.\n".
+                "User Details: $message .\n";
+
+$to = 'info@shaikalthaf.in';
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: 3dprint.html");
+
+
+?>
